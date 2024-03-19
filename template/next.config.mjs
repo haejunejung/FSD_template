968a5a2c36c095641aac5f2c771de570ce8d3017
haejunejung <jhj9422@unist.ac.kr>
2024-03-19 14:45:07 +0900

    nextjs14 emotion 추가

--- template/next.config.mjs
+++ template/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  compiler: {
+    // https://nextjs.org/docs/architecture/nextjs-compiler#emotion
+    emotion: {
+      sourceMap: true,
+      autoLabel: "always",
+      labelFormat: "[local]",
+    },
+  },
+};
 
 export default nextConfig;
