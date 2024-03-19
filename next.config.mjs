3c7abce2dc8091dfc3b2fdba4c0cbe7987102549
haejunejung <jhj9422@unist.ac.kr>
2024-03-19 18:51:52 +0900

    chore : emotion

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const nextConfig = {
+  compiler: {
+    // https://nextjs.org/docs/architecture/nextjs-compiler#emotion
+    emotion: {
+      sourceMap: true,
+      autoLabel: 'always',
+      labelFormat: '[local]',
+    },
+  },
+};
